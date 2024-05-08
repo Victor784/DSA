84eb637c5468f3c5e91244abdeefefa08ff021a2
Victor784 <victorpop839@yahoo.com>
2024-05-08 06:07:09 +0300

    DSA-13: Implemented Singly linked list

--- DataStructures/LinkedLists/SinglyLinkedList.hpp
+++ DataStructures/LinkedLists/SinglyLinkedList.hpp
@@ -1,9 +1,245 @@
 #pragma once
 
+#include <cstddef>
+#include <iterator>
+#include <mutex>
+#include <optional>
+#include <stdexcept>
+
 namespace vics_data_structures {
+
+using index_t = size_t;
 
 namespace linked_lists {
 
-namespace singly_linked_list {}
+namespace singly_linked_list {
+
+template <typename T> class SinglyLinkedList {
+
+private:
+  template <typename T> struct Node {
+  public:
+    std::optional<T> data;
+    Node<T> *next;
+    Node(const T &data) : data(std::make_optional(data)), next(nullptr) {}
+
+    friend bool operator==(const Node<T> &lhs, const Node<T> &rhs) {
+      return lhs.data == rhs.data && lhs.next == rhs.next;
+    }
+  };
+
+  // iterator class so that std::find and other algs from std lib work with this
+  // DS, and for easier transversal
+public:
+  template <typename T> struct Iterator {
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = Node<T>;
+    using pointer = Node<T> *;   // or also value_type*
+    using reference = Node<T> &; // or also value_type&
+
+    Iterator(pointer ptr) : m_ptr(ptr) {}
+
+    reference operator*() const { return *m_ptr; }
+    pointer operator->() { return m_ptr; }
+
+    Iterator &operator++() {
+      m_ptr = m_ptr->next;
+      return *this;
+    }
+
+    Iterator operator++(int) {
+      Iterator tmp = *this;
+      ++(*this);
+      return tmp;
+    }
+
+    friend bool operator==(const Iterator &a, const Iterator &b) {
+      return a.m_ptr == b.m_ptr;
+    };
+    friend bool operator!=(const Iterator &a, const Iterator &b) {
+      return a.m_ptr != b.m_ptr;
+    };
+
+  private:
+    pointer m_ptr;
+  };
+
+public:
+  // Constructors + Destructor definitions
+  SinglyLinkedList() = default;
+
+  SinglyLinkedList(const SinglyLinkedList &other) = delete;
+  SinglyLinkedList &operator=(const SinglyLinkedList &other) = delete;
+
+  SinglyLinkedList(SinglyLinkedList &&other)
+      : m_head(other.getHeadNode()), m_tail(other.getTailNode()),
+        m_size(other.getSize()) {
+    other.shallowClear();
+  }
+  SinglyLinkedList &operator=(SinglyLinkedList &&other) {
+    if (this != &other) {
+      clear();
+      m_head = other.getHeadNode();
+      m_tail = other.getTailNode();
+      m_size = other.getSize();
+      other.shallowClear();
+    }
+    return *this;
+  }
+  ~SinglyLinkedList() { clear(); }
+
+  // SingltLinkedList Interface
+  void shallowClear() {
+    m_head = nullptr;
+    m_tail = nullptr;
+    m_size = 0;
+  }
+  Node<T> *getHeadNode() const { return m_head; }
+  Node<T> *getTailNode() const { return m_tail; }
+  size_t getSize() const { return m_size; }
+
+  void clear() {
+    std::lock_guard<std::mutex> lock(m_mutex);
+
+    Node<T> *current = m_head;
+    while (current) {
+      Node<T> *next = current->next;
+      delete current;
+      current = next;
+    }
+    m_head = nullptr;
+    m_tail = nullptr;
+    m_size = 0;
+  }
+
+  Iterator<T> begin() { return Iterator<T>(m_head); }
+
+  Iterator<T> end() { return Iterator<T>(m_tail); }
+
+  void push_front(const T &value) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    Node<T> *newNode = new Node<T>(value);
+    if (!m_head)
+      m_head = m_tail = newNode;
+    else {
+      newNode->next = m_head;
+      m_head = newNode;
+    }
+    m_size++;
+  }
+  void push_back(const T &value) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    Node<T> *newNode = new Node<T>(value);
+    if (!m_tail)
+      m_head = m_tail = newNode;
+    else {
+      m_tail->next = newNode;
+      m_tail = newNode;
+    }
+    m_size++;
+  }
+  void insert(const T &value, index_t pos) {
+    if (pos == 0) {
+      push_front(value);
+    } else if (pos >= m_size) {
+      push_back(value);
+    } else {
+      std::lock_guard<std::mutex> lock(m_mutex);
+      Node<T> *newNode = new Node<T>(value);
+      Node<T> *nodeBeforeInsertion = m_head;
+      for (size_t i = 0; i < pos - 1; ++i) {
+        nodeBeforeInsertion = nodeBeforeInsertion->next;
+      }
+      newNode->next = nodeBeforeInsertion->next;
+      nodeBeforeInsertion->next = newNode;
+      m_size++;
+    }
+  }
+  std::optional<T> pop_front() {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    if (!m_head)
+      return std::nullopt;
+    Node<T> *temp = m_head;
+    m_head = m_head->next;
+    const auto data = temp->data;
+    delete temp;
+    if (!m_head)
+      m_tail = nullptr;
+    m_size--;
+    return data;
+  }
+  std::optional<T> pop_back() {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    if (!m_head)
+      return std::nullopt;
+    if (m_head == m_tail) {
+      const auto data = m_head->data;
+      delete m_head;
+      m_head = m_tail = nullptr;
+      m_size = 0;
+      return data;
+    }
+    Node<T> *current = m_head;
+    while (current->next != m_tail) {
+      current = current->next;
+    }
+    const auto data = m_tail->data;
+    delete m_tail;
+    m_tail = current;
+    m_tail->next = nullptr;
+    m_size--;
+    return data;
+  }
+  void erase(index_t pos) {
+    if (!m_head)
+      throw std::out_of_range("Index out of bounds");
+    if (pos == 0) {
+      pop_front();
+    } else if (pos >= m_size - 1) {
+      pop_back();
+    } else {
+      std::lock_guard<std::mutex> lock(m_mutex);
+      Node<T> *nodeHeforeDeletion = m_head;
+      for (size_t i = 0; i < pos - 1; ++i) {
+        nodeHeforeDeletion = nodeHeforeDeletion->next;
+      }
+      Node<T> *temp = nodeHeforeDeletion->next;
+      nodeHeforeDeletion->next = temp->next;
+      delete temp;
+      m_size--;
+    }
+  }
+  std::optional<T> front() {
+    if (!m_head)
+      return std::nullopt;
+    return m_head->data;
+  }
+  std::optional<T> back() {
+    if (!m_tail)
+      return std::nullopt;
+    return m_tail->data;
+  }
+
+  std::optional<T> operator[](index_t index) const {
+    if (index >= m_size) {
+      throw std::out_of_range("Index out of bounds");
+    }
+    Node<T> *current = m_head;
+    for (size_t i = 0; i < index; i++) {
+      current = current->next;
+    }
+    return current->data;
+  }
+  bool is_empty() { return m_size == 0; }
+
+private:
+  Node<T> *m_head = nullptr;
+  Node<T> *m_tail = nullptr;
+  size_t m_size = 0;
+  mutable std::mutex m_mutex;
+};
+
+} // namespace singly_linked_list
 } // namespace linked_lists
 } // namespace vics_data_structures
--- DataStructures/test/LinkedListsTest.cpp
+++ DataStructures/test/LinkedListsTest.cpp
@@ -1,6 +1,7 @@
 #include "DoublyLinkedList.hpp"
 #include "SinglyLinkedList.hpp"
 #include "gtest/gtest.h"
+#include <algorithm>
 
 using namespace vics_data_structures;
 namespace tests {
@@ -22,162 +23,355 @@ private:
 };
 } // namespace custom_types
 
-TEST(LinkedListsTest, Test1) {
-  //   vics_data_structures::arrays::DynamicArray<int> arr;
-  //   ASSERT_EQ(arr.getCapacity(), 10); // Test that initial capacity is 10s
-  //   EXPECT_EQ(arr.getLength(), 0);    // Init the size ptr is at the
-  //   beggining size_t initialSize = 0; for (int i = 0; i < arr.getLength();
-  //   ++i) {
-  //     initialSize += sizeof(arr.get(i));
-  //   }
-  //   EXPECT_LE(initialSize,
-  //             sizeof(size_t) * 2 +
-  //                 sizeof(int) *
-  //                     10); // Espect that the object is smaller than the sum
-  //                     of
-  //                          // all its contents as the m_data is saved on heap
-  //   EXPECT_TRUE(arr.is_empty());
+using namespace vics_data_structures::linked_lists;
+
+TEST(LinkedListsTest,
+     SinglyLinkedList_Default_constructor_constructs_emepty_list) {
+  singly_linked_list::SinglyLinkedList<int> list;
+
+  // Verify that the list is empty after construction
+  ASSERT_TRUE(list.is_empty());
+  ASSERT_EQ(list.getSize(), 0);
 }
 
-TEST(LinkedListsTest, Test2) {
-  //   vics_data_structures::arrays::DynamicArray<char> arr;
-  //   ASSERT_EQ(arr.getCapacity(), 10); // Test that initial capacity is 10s
-  //   EXPECT_EQ(arr.getLength(), 0);    // Init the size ptr is at the
-  //   beggining size_t initialSize = 0; for (int i = 0; i < arr.getLength();
-  //   ++i) {
-  //     initialSize += sizeof(arr.get(i));
-  //   }
-  //   EXPECT_LE(initialSize,
-  //             sizeof(size_t) * 2 +
-  //                 sizeof(char) *
-  //                     10); // Espect that the object is smaller than the sum
-  //                     of
-  //                          // all its contents as the m_data is saved on heap
-  //   EXPECT_TRUE(arr.is_empty());
+TEST(LinkedListsTest,
+     SinglyLinkedList_Move_constructor_constructs_another_list_correctly) {
+  singly_linked_list::SinglyLinkedList<int> list1;
+  list1.push_back(1);
+  list1.push_back(2);
+  singly_linked_list::SinglyLinkedList<int> list2(std::move(list1));
+
+  // Verify that list2 has taken ownership of the elements from list1
+  ASSERT_TRUE(list1.is_empty());
+  ASSERT_TRUE(list2.front().has_value());
+  ASSERT_EQ(list2.getSize(), 2);
 }
 
-TEST(LinkedListsTest, Test3) {
-  // vics_data_structures::arrays::DynamicArray<custom_types::Point> arr;
-  // ASSERT_EQ(arr.getCapacity(), 10); // Test that initial capacity is 10s
-  //   EXPECT_EQ(arr.getLength(), 0);    // Init the size ptr is at the
-  //   beggining size_t initialSize = 0; for (int i = 0; i < arr.getLength();
-  //   ++i) {
-  //     initialSize += sizeof(arr.get(i));
-  //   }
-  //   EXPECT_LE(initialSize,
-  //             sizeof(size_t) * 2 +
-  //                 sizeof(custom_types::Point) *
-  //                     10); // Espect that the object is smaller than the sum
-  //                     of
-  //                          // all its contents as the m_data is saved on heap
-  //   EXPECT_TRUE(arr.is_empty());
+TEST(
+    LinkedListsTest,
+    SinglyLinkedList_Move_assignment_constructor_constructs_another_list_correctly) {
+  // Create a source SinglyLinkedList
+  singly_linked_list::SinglyLinkedList<int> sourceList;
+  sourceList.push_back(1);
+  sourceList.push_back(2);
+  sourceList.push_back(3);
+
+  // Create a destination SinglyLinkedList
+  singly_linked_list::SinglyLinkedList<int> destinationList;
+  destinationList.push_back(4);
+  destinationList.push_back(5);
+
+  // Perform move assignment
+  destinationList = std::move(sourceList);
+
+  // Check if the destination list has been properly moved
+  ASSERT_EQ(destinationList.getSize(), 3); // Check if size is 3
+  ASSERT_EQ(destinationList.front(), 1);   // Check if first element is 1
+  ASSERT_EQ(destinationList.back(), 3);    // Check if last element is 3
+
+  // Check if the source list is now empty
+  ASSERT_EQ(sourceList.getSize(), 0);
 }
 
-TEST(LinkedListsTest, Test4) {
-  //   vics_data_structures::arrays::DynamicArray<int> arr;
-  //   ASSERT_EQ(arr.getCapacity(), 10);
-  //   EXPECT_EQ(arr.getLength(), 0);
-  //   EXPECT_TRUE(arr.is_empty());
-
-  //   arr.push_back(std::make_optional(1));
-  //   EXPECT_EQ(arr.getLength(), 1);
-  //   ASSERT_EQ(arr.getCapacity(), 10);
-  //   EXPECT_EQ(arr.get(0), 1);
-
-  //   arr.push_back(std::make_optional(2));
-  //   EXPECT_EQ(arr.getLength(), 2);
-  //   ASSERT_EQ(arr.getCapacity(), 10);
-  //   EXPECT_EQ(arr.get(0), 1);
-  //   EXPECT_EQ(arr.get(1), 2);
-
-  //   arr.push_back(std::make_optional(3));
-  //   arr.push_back(std::make_optional(4));
-  //   arr.push_back(std::make_optional(5));
-  //   arr.push_back(std::make_optional(6));
-  //   arr.push_back(std::make_optional(7));
-  //   arr.push_back(std::make_optional(8));
-  //   arr.push_back(std::make_optional(9));
-  //   EXPECT_EQ(arr.getLength(), 9);
-  //   ASSERT_EQ(arr.getCapacity(), 10);
-  //   arr.push_back(std::make_optional(10));
-  //   arr.push_back(std::make_optional(11));
-  //   EXPECT_EQ(arr.getLength(), 11);
-  //   ASSERT_EQ(arr.getCapacity(), 20);
+TEST(
+    LinkedListsTest,
+    SinglyLinkedList_Default_constructor_with_custom_type_works_just_as_intended) {
+  singly_linked_list::SinglyLinkedList<custom_types::Point> list;
+  ASSERT_TRUE(list.is_empty());
+  ASSERT_EQ(list.getSize(), 0);
+  custom_types::Point point1{1, 2};
+  custom_types::Point point2{3, 4};
+  custom_types::Point point3{5, 6};
+  list.push_back(point1);
+  list.push_back(point2);
+  list.push_back(point3);
+  ASSERT_EQ(list.getSize(), 3);
+  ASSERT_EQ(list.getHeadNode()->data.value().getX(), 1);
+  ASSERT_EQ(list.getHeadNode()->data.value().getY(), 2);
+  ASSERT_EQ(list.getTailNode()->data.value().getX(), 5);
+  ASSERT_EQ(list.getTailNode()->data.value().getY(), 6);
 }
 
-TEST(LinkedListsTest, Test5) {
-  //   vics_data_structures::arrays::DynamicArray<int> arr;
-  //   ASSERT_EQ(arr.getCapacity(), 10);
-  //   EXPECT_EQ(arr.getLength(), 0);
-  //   EXPECT_TRUE(arr.is_empty());
-
-  //   EXPECT_EQ(arr.pop_back(), std::nullopt);
-  //   EXPECT_EQ(arr.getLength(), 0);
-
-  //   arr.push_back(std::make_optional(1));
-  //   EXPECT_EQ(arr.getLength(), 1);
-  //   arr.push_back(std::make_optional(2));
-  //   EXPECT_EQ(arr.getLength(), 2);
-  //   arr.push_back(std::make_optional(3));
-  //   arr.push_back(std::make_optional(4));
-  //   arr.push_back(std::make_optional(5));
-  //   arr.push_back(std::make_optional(6));
-  //   arr.push_back(std::make_optional(7));
-  //   arr.push_back(std::make_optional(8));
-  //   arr.push_back(std::make_optional(9));
-  //   arr.push_back(std::make_optional(10));
-  //   arr.push_back(std::make_optional(11));
-  //   EXPECT_EQ(arr.getLength(), 11);
-  //   ASSERT_EQ(arr.getCapacity(), 20);
-  //   EXPECT_FALSE(arr.is_empty());
-
-  //   const auto ret = arr.pop_back();
-  //   ASSERT_EQ(arr.getLength(), 10);
-  //   EXPECT_EQ(ret, 11);
-  //   ASSERT_EQ(arr.getCapacity(), 20); // capacity does not change
+TEST(
+    LinkedListsTest,
+    SinglyLinkedList_get_head_node_returns_first_element_in_list_and_get_tail_node_retunrs_last) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  ASSERT_EQ(list.getSize(), 3);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 1);
+  EXPECT_EQ(list.getTailNode()->data.value(), 3);
 }
 
-TEST(LinkedListsTest, Test6) {
-  // TODO uncomment test and run it after multithreadding safety implemented
-  // vics_data_structures::arrays::DynamicArray<int> arr;
-  // ASSERT_EQ(arr.getCapacity(), 10); // Test that initial capacity is 10s
-  // EXPECT_EQ(arr.getLength(), 0);    // Init the size ptr is at the
-  // EXPECT_TRUE(arr.is_empty());
-  // arr.set(std::make_optional(1), index_t(10));
-  // EXPECT_EQ(arr.getLength(), 10);
-  // EXPECT_EQ(arr.getCapacity(), 20);
-  // EXPECT_FALSE(arr.is_empty());
-  // EXPECT_EQ(arr[0], std::nullopt);
-  // EXPECT_EQ(arr[1], std::nullopt);
-  // EXPECT_EQ(arr[2], std::nullopt);
-  // EXPECT_EQ(arr[3], std::nullopt);
-  // EXPECT_EQ(arr[4], std::nullopt);
-  // EXPECT_EQ(arr[5], std::nullopt);
-  // EXPECT_EQ(arr[6], std::nullopt);
-  // EXPECT_EQ(arr[7], std::nullopt);
-  // EXPECT_EQ(arr[8], std::nullopt);
-  // EXPECT_EQ(arr[9], std::nullopt);
+TEST(
+    LinkedListsTest,
+    SinglyLinkedList_clear_removes_all_elements_and_sets_extreme_nodes_to_null) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  ASSERT_EQ(list.getSize(), 3);
+  list.clear();
+  EXPECT_EQ(list.getSize(), 0);
+  ASSERT_EQ(list.getHeadNode(), nullptr);
+  ASSERT_EQ(list.getTailNode(), nullptr);
 }
 
-TEST(LinkedListsTest, Test7) {
-  // vics_data_structures::arrays::DynamicArray<int> arr;
-  // TODO uncomment test and run it after multithreadding safety implemented
-  // ASSERT_EQ(arr.getCapacity(), 10); // Test that initial capacity is 10s
-  // EXPECT_EQ(arr.getLength(), 0);    // Init the size ptr is at the
-  // beggining
-  // EXPECT_TRUE(arr.is_empty());
-  // arr.set(std::make_optional(1), index_t(4));
-  // EXPECT_EQ(arr.getLength(), 5);
-  // EXPECT_EQ(arr.getCapacity(), 10);
-  // EXPECT_FALSE(arr.is_empty());
-  // EXPECT_EQ(arr[0], std::nullopt);
-  // EXPECT_EQ(arr[1], std::nullopt);
-  // EXPECT_EQ(arr[2], std::nullopt);
-  // EXPECT_EQ(arr[3], std::nullopt);
-  // EXPECT_EQ(arr[4], 1);
-  // EXPECT_THROW({ arr.get(5); }, std::out_of_range);
-  // EXPECT_EQ(arr.get(0), std::nullopt);
-  // EXPECT_EQ(arr.get(4), 1);
+TEST(LinkedListsTest, SinglyLinkedList_begin_returns_interator_for_head_node) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  // EXPECT_EQ(list.begin()->data.value(), 1);
+  EXPECT_EQ(list.begin(), list.getHeadNode());
+}
+TEST(LinkedListsTest, SinglyLinkedList_end_returns_interator_over_tail_node) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  EXPECT_EQ(list.end(), list.getTailNode());
+}
+TEST(LinkedListsTest,
+     SinglyLinkedList_push_front_inserts_new_node_with_empty_list) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_front(1);
+  ASSERT_EQ(list.getSize(), 1);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 1);
+  EXPECT_EQ(list.getTailNode()->data.value(), 1);
+}
+TEST(
+    LinkedListsTest,
+    SinglyLinkedList_push_front_inserts_new_node_before_head_with_non_empty_list) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  ASSERT_EQ(list.getSize(), 2);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 1);
+  list.push_front(3);
+  EXPECT_EQ(list.getSize(), 3);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 3);
+}
+TEST(LinkedListsTest,
+     SinglyLinkedList_push_back_inserts_new_node_with_empty_list) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  ASSERT_EQ(list.getSize(), 1);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 1);
+  EXPECT_EQ(list.getTailNode()->data.value(), 1);
+}
+TEST(
+    LinkedListsTest,
+    SinglyLinkedList_push_back_inserts_new_node_after_tail_with_non_empty_list) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_front(1);
+  list.push_front(2);
+  ASSERT_EQ(list.getSize(), 2);
+  EXPECT_EQ(list.getTailNode()->data.value(), 1);
+  list.push_back(3);
+  EXPECT_EQ(list.getSize(), 3);
+  EXPECT_EQ(list.getTailNode()->data.value(), 3);
+}
+TEST(LinkedListsTest, SinglyLinkedList_insert_inserts_new_node_on_valid_index) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  ASSERT_EQ(list.getSize(), 2);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 1);
+  EXPECT_EQ(list.getTailNode()->data.value(), 2);
+  list.insert(3, 1);
+  ASSERT_EQ(list.getSize(), 3);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 1);
+  EXPECT_EQ(list.getTailNode()->data.value(), 2);
+}
+
+TEST(LinkedListsTest,
+     SinglyLinkedList_insert_inserts_new_node_before_head_on_index_eq_0) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  ASSERT_EQ(list.getSize(), 2);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 1);
+  EXPECT_EQ(list.getTailNode()->data.value(), 2);
+  list.insert(3, 0);
+  ASSERT_EQ(list.getSize(), 3);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 3);
+  EXPECT_EQ(list.getTailNode()->data.value(), 2);
+}
+TEST(
+    LinkedListsTest,
+    SinglyLinkedList_insert_inserts_new_node_after_tail_on_index_higher_than_size) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  ASSERT_EQ(list.getSize(), 2);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 1);
+  EXPECT_EQ(list.getTailNode()->data.value(), 2);
+  list.insert(3, 3);
+  ASSERT_EQ(list.getSize(), 3);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 1);
+  EXPECT_EQ(list.getTailNode()->data.value(), 3);
+
+  list.insert(4, 4);
+  ASSERT_EQ(list.getSize(), 4);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 1);
+  EXPECT_EQ(list.getTailNode()->data.value(), 4);
+
+  list.insert(5, 7);
+  ASSERT_EQ(list.getSize(), 5);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 1);
+  EXPECT_EQ(list.getTailNode()->data.value(), 5);
+}
+TEST(LinkedListsTest,
+     SinglyLinkedList_pop_front_returns_null_opt_for_empty_list) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  ASSERT_EQ(list.getSize(), 0);
+  const auto ret = list.pop_front();
+  EXPECT_EQ(ret, std::nullopt);
+}
+TEST(LinkedListsTest,
+     SinglyLinkedList_pop_front_deletes_head_node_and_returns_its_value) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  ASSERT_EQ(list.getSize(), 3);
+  const auto ret = list.pop_front();
+  ASSERT_EQ(list.getSize(), 2);
+  EXPECT_EQ(ret.value(), 1);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 2);
+}
+TEST(LinkedListsTest,
+     SinglyLinkedList_pop_back_returns_null_opt_for_empty_list) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  ASSERT_EQ(list.getSize(), 0);
+  const auto ret = list.pop_back();
+  EXPECT_EQ(ret, std::nullopt);
+}
+TEST(LinkedListsTest,
+     SinglyLinkedList_pop_back_deletes_tail_node_and_returns_its_value) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  ASSERT_EQ(list.getSize(), 3);
+  const auto ret = list.pop_back();
+  ASSERT_EQ(list.getSize(), 2);
+  EXPECT_EQ(ret.value(), 3);
+  EXPECT_EQ(list.getTailNode()->data.value(), 2);
+}
+TEST(LinkedListsTest, SinglyLinkedList_erase_deletes_head_node_for_index_eq_0) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  ASSERT_EQ(list.getSize(), 3);
+  list.erase(0);
+  ASSERT_EQ(list.getSize(), 2);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 2);
+}
+TEST(LinkedListsTest,
+     SinglyLinkedList_erase_deletes_tail_node_for_index_eq_size) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  ASSERT_EQ(list.getSize(), 3);
+  list.erase(2);
+  ASSERT_EQ(list.getSize(), 2);
+  EXPECT_EQ(list.getTailNode()->data.value(), 2);
+}
+TEST(LinkedListsTest, SinglyLinkedList_erase_throws_for_empty_list) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  ASSERT_EQ(list.getSize(), 0);
+  EXPECT_THROW(list.erase(0), std::out_of_range);
+}
+
+TEST(LinkedListsTest, SinglyLinkedList_erase_deletes_node_at_index) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  ASSERT_EQ(list.getSize(), 3);
+  list.erase(1);
+  ASSERT_EQ(list.getSize(), 2);
+  EXPECT_EQ(list.getHeadNode()->data.value(), 1);
+  EXPECT_EQ(list.getTailNode()->data.value(), 3);
+}
+TEST(LinkedListsTest, SinglyLinkedList_front_returns_data_from_head_node) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  ASSERT_EQ(list.getSize(), 3);
+  const auto front = list.front().value();
+  EXPECT_EQ(front, 1);
+}
+TEST(LinkedListsTest, SinglyLinkedList_back_returns_data_from_tail_node) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  ASSERT_EQ(list.getSize(), 3);
+  const auto back = list.back().value();
+  EXPECT_EQ(back, 3);
+}
+TEST(LinkedListsTest,
+     SinglyLinkedList_square_brackets_operator_trows_for_index_out_of_range) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  ASSERT_EQ(list.getSize(), 3);
+  EXPECT_THROW(list[4], std::out_of_range);
+}
+TEST(LinkedListsTest,
+     SinglyLinkedList_square_brackets_operator_returns_data_at_index) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  ASSERT_EQ(list.getSize(), 3);
+  EXPECT_EQ(list[0].value(), 1);
+  EXPECT_EQ(list[1].value(), 2);
+  EXPECT_EQ(list[2].value(), 3);
+}
+TEST(
+    LinkedListsTest,
+    SinglyLinkedList_is_empty_returns_true_if_list_is_empty_and_false_otherwise) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  EXPECT_TRUE(list.is_empty());
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  EXPECT_FALSE(list.is_empty());
+}
+
+TEST(LinkedListsTest, SinglyLinkedList_iterator_can_be_used_to_navigate_list) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  EXPECT_TRUE(list.is_empty());
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+
+  ASSERT_EQ(list.getSize(), 3);
+  ASSERT_EQ(list[0].value(), 1);
+  ASSERT_EQ(list[1].value(), 2);
+  ASSERT_EQ(list[2].value(), 3);
+  auto it = list.begin();
+  ASSERT_EQ(it->data, 1);
+  it++;
+  ASSERT_EQ(it->data, 2);
+  it++;
+  ASSERT_EQ(it->data, 3);
+}
+
+TEST(LinkedListsTest, SinglyLinkedList_can_be_used_in_std_find_alg) {
+  singly_linked_list::SinglyLinkedList<int> list;
+  EXPECT_TRUE(list.is_empty());
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  auto result = std::find(list.begin(), list.end(), *list.getHeadNode());
+  ASSERT_NE(result, list.end());
+  ASSERT_EQ(result->data.value(), 1);
+  result = std::find(list.begin(), list.end(), *(list.getTailNode() + 1));
+  ASSERT_EQ(result, list.end());
 }
 
 } // namespace test_construct
